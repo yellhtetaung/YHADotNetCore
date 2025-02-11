a2450573f44cd451c785fa904d34d85e1d6fe52c
yellhtetaung <yehtetaung.dev@gmail.com>
2025-02-11 17:50:31 +0630

    Day 1

--- YHADotNetCore.ConsoleApp/Program.cs
+++ YHADotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using System.Data.SqlClient;
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "DESKTOP-0RPA0T5";
+stringBuilder.InitialCatalog = "DotNetTrainingBatch4";
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "Yehtetaung@2481998";
+
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+connection.Open();
+Console.WriteLine("Connection open.");
+
+string query = "SELECT * FROM Tbl_Blog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+
+connection.Close();
+Console.WriteLine("Connection close.");
+
+// dataset => datatable
+// datatable => datarow
+// datarow => datacolumn
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog Id => " + dr["BlogId"]);
+    Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+    Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+    Console.WriteLine("---------------------------------------");
+}
+
+// Ado.Net Read
+
+Console.ReadKey();
