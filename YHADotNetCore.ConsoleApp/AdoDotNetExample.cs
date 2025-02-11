1a04bd07c1c94a4c0f22b669deb402a000bdf68f
yellhtetaung <yehtetaung.dev@gmail.com>
2025-02-11 18:07:30 +0630

    day_02: finished create method

---
+++ YHADotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YHADotNetCore.ConsoleApp
+{
+    internal class AdoDotNetExample
+    {
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "DESKTOP-0RPA0T5",
+            InitialCatalog = "DotNetTrainingBatch4",
+            UserID = "sa",
+            Password = "Yehtetaung@2481998"
+        };
+
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            Console.WriteLine("Connection open.");
+
+            string query = "SELECT * FROM Tbl_Blog";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+            Console.WriteLine("Connection close.");
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("Blog Id => " + dr["BlogId"]);
+                Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+                Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+                Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+                Console.WriteLine("---------------------------------------");
+            }
+        }
+
+        public void Create(string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+		   ,@BlogAuthor
+		   ,@BlogContent)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            string message = result > 0 ? "Saving Successful." : "Saving Failed";
+            Console.WriteLine(message);
+        }
+    }
+}
--- YHADotNetCore.ConsoleApp/Program.cs
+++ YHADotNetCore.ConsoleApp/Program.cs
@@ -1,38 +1,44 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using YHADotNetCore.ConsoleApp;
 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = "DESKTOP-0RPA0T5";
-stringBuilder.InitialCatalog = "DotNetTrainingBatch4";
-stringBuilder.UserID = "sa";
-stringBuilder.Password = "Yehtetaung@2481998";
+//SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+//stringBuilder.DataSource = "DESKTOP-0RPA0T5";
+//stringBuilder.InitialCatalog = "DotNetTrainingBatch4";
+//stringBuilder.UserID = "sa";
+//stringBuilder.Password = "Yehtetaung@2481998";
 
-SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
-connection.Open();
-Console.WriteLine("Connection open.");
+//SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+//connection.Open();
+//Console.WriteLine("Connection open.");
 
-string query = "SELECT * FROM Tbl_Blog";
-SqlCommand cmd = new SqlCommand(query, connection);
-SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-DataTable dt = new DataTable();
-sqlDataAdapter.Fill(dt);
+//string query = "SELECT * FROM Tbl_Blog";
+//SqlCommand cmd = new SqlCommand(query, connection);
+//SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+//DataTable dt = new DataTable();
+//sqlDataAdapter.Fill(dt);
 
-connection.Close();
-Console.WriteLine("Connection close.");
+//connection.Close();
+//Console.WriteLine("Connection close.");
 
-// dataset => datatable
-// datatable => datarow
-// datarow => datacolumn
+//// dataset => datatable
+//// datatable => datarow
+//// datarow => datacolumn
 
-foreach (DataRow dr in dt.Rows)
-{
-    Console.WriteLine("Blog Id => " + dr["BlogId"]);
-    Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
-    Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
-    Console.WriteLine("Blog Content => " + dr["BlogContent"]);
-    Console.WriteLine("---------------------------------------");
-}
+//foreach (DataRow dr in dt.Rows)
+//{
+//    Console.WriteLine("Blog Id => " + dr["BlogId"]);
+//    Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+//    Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+//    Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+//    Console.WriteLine("---------------------------------------");
+//}
 
 // Ado.Net Read
+// CRUD
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.Read();
+//adoDotNetExample.Create("title", "author", "content");
 
 Console.ReadKey();
